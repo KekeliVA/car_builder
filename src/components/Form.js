c6da4ca70efd97e27ce531910074c9bd0065001f
KekeliVA <kekeli.afantchao@yahoo.com>
2021-05-25 13:57:13 -0400

    created first iteration of form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 export default FormData = () => {
-    console.log("start of the form component");
-    return (
-        <form>
-            <input id="car_make"></input>
-            <input id="car_model"></input>
-            <button type="submit"></button>
-        </form>
-    )
-}
+  console.log("start of the form component");
+  return (
+    <form>
+      <div>
+        <label for="car_make">What is the car make? </label>
+        <input name="car_make"></input>
+      </div>
+
+      <div>
+        <label for="car_model">What is the car model? </label>
+        <input id="car_model"></input>
+      </div>
+      <button type="submit">submit</button>
+    </form>
+  );
+};
